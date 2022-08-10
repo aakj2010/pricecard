ecd40263ce0f54a847f7ceca0984ae80f7f79cee
aakj2010 <aakj2010@gmail.com>
2022-08-10 17:22:34 +0530

    new1

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import './App.css';
+import Card from './Card';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  const data = [
+    {
+      name: "Free",
+      price: "0",
+      currency : "$",
+      period: "month",
+      features: [
+        {
+          title: "Single User",
+          isEnable: true
+        },
+        {
+          title: "5GB Storage",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Public Projects",
+          isEnable: true
+        },
+        {
+          title: "Community Access",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Private Projects",
+          isEnable: false
+        },
+        {
+          title: "Dedicated Phone Support",
+          isEnable: false
+        },
+        {
+          title: "Free Subdomain",
+          isEnable: false
+        },
+        {
+          title: "Monthly Status Reports",
+          isEnable: false
+        }
+      ]
+    },
+    {
+      name : "PLUS",
+      price: "9",
+      currency :"$",
+      period: "month",
+      features:[
+        {
+          title: "5 User",
+          isEnable: true,
+          isBold : true
+        },
+        {
+          title: "50GB Storage",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Public Projects",
+          isEnable: true
+        },
+        {
+          title: "Community Access",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Private Projects",
+          isEnable: true
+        },
+        {
+          title: "Dedicated Phone Support",
+          isEnable: true
+        },
+        {
+          title: "Free Subdomain",
+          isEnable: true
+        },
+        {
+          title: "Monthly Status Reports",
+          isEnable: false
+        }
+      ]
+    },
+    {
+      name : "PRO",
+      price: "49",
+      currency : '$',
+      period: "month",
+      features:[
+        {
+          title: "Unlimited User",
+          isEnable: true,
+          isBold : true
+        },
+        {
+          title: "150GB Storage",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Public Projects",
+          isEnable: true
+        },
+        {
+          title: "Community Access",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Private Projects",
+          isEnable: true
+        },
+        {
+          title: "Dedicated Phone Support",
+          isEnable: true
+        },
+        {
+          title: "Unlimited Free Subdomain",
+          isEnable: true
+        },
+        {
+          title: "Monthly Status Reports",
+          isEnable: true
+        }
+      ]
+    }
+    
+  ]
+  return (
+      <section class="pricing py-5">
+        <div class="container">
+          <div class="row">
+            {
+              data.map((item)=>{
+                return <Card item={item}></Card>
+              })
+            }
+          </div>
+        </div>
+      </section>
+  );
+}
+
+export default App;
